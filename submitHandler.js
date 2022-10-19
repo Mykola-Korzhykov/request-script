547dd6b0b0cb41cdf185137b9fda1fff76fb817d
Nikolay Korzhikov <45668685+Lil-Korzhik@users.noreply.github.com>
2022-10-19 11:02:33 +0300

    Update submitHandler.js

--- submitHandler.js
+++ submitHandler.js
@@ -1,13 +1,15 @@
-const form = document.getElementById('form');
+const form = document.getElementById(`${scriptConfig.fieldTag}-form`);
 
-form.addEventListener('submit', e => {
-    const fields = collectFields();
-    const isValidate = validateFields(fields);
+if(form) {
+    form.addEventListener('submit', e => {
+        const fields = collectFields();
+        const isValidate = validateFields(fields);
 
-    if(isValidate) {
-        const message = createMessage(fields);
-        sendMessage(message);
-    }
+        if(isValidate) {
+            const message = createMessage(fields);
+            sendMessage(message);
+        }
 
-    e.preventDefault();
-});
+        e.preventDefault();
+    });
+}
