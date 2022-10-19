6eae2203b4601a8e2b36774ab29074e4a30066ed
Nikolay Korzhikov <45668685+Lil-Korzhik@users.noreply.github.com>
2022-10-19 12:01:25 +0300

    Update validateFields.js

--- functions/validateFields.js
+++ functions/validateFields.js
@@ -2,7 +2,7 @@ const validateFields = fields => {
     for(key in fields) {
         const field = fields[key];
         if(!field.value || !field.text || !field.smile) {
-            console.log('Some fields is empty or wrong!');
+            if(scriptConfig.env == 'dev') console.log('Some fields is empty or wrong!');
             return false;
         }
     }
